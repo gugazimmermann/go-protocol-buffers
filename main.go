c52676d7bc14d86b3a8223a6656feb081668ba99
Guga Zimmermann <gugazimmermann@gmail.com>
2021-03-18 15:31:57 -0300

    Enum

--- main.go
+++ main.go
@@ -5,35 +5,13 @@ import (
 	"io/ioutil"
 	"log"
 
+	"github.com/gugazimmermann/go-protocol-buffers/src/enum_example/enumpb"
 	"github.com/gugazimmermann/go-protocol-buffers/src/simple/simplepb"
 	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
 )
 
-func main() {
-	sm := doSimple()
-	readAndWriteSimple(sm)
-	fmt.Println("")
-	jsonDemoSimple(sm)
-}
-
-func readAndWriteSimple(sm proto.Message) {
-	writeToFile("simple.bin", sm)
-	sm2 := &simplepb.SimpleMessage{}
-	readFromFIle("simple.bin", sm2)
-	fmt.Println("PB from simple.bin:", sm2)
-}
-
-func jsonDemoSimple(sm proto.Message) {
-	smAsJSONString := toJson(sm)
-	fmt.Println("PB to JSON:")
-	fmt.Println(string(smAsJSONString))
-	sm2 := &simplepb.SimpleMessage{}
-	fromJson(smAsJSONString, sm2)
-	fmt.Println("PB from JSON:", sm2)
-}
-
-func doSimple() proto.Message {
+func doSimple() *simplepb.SimpleMessage {
 	sm := simplepb.SimpleMessage{
 		Id:         12345,
 		IsSimple:   true,
@@ -43,23 +21,66 @@ func doSimple() proto.Message {
 	return &sm
 }
 
-func writeToFile(fName string, pb proto.Message) error {
+func doEnum(ID int32) *enumpb.EnumMessage {
+	en := enumpb.EnumMessage{
+		Id: ID,
+	}
+	return &en
+}
+
+func main() {
+	// simple proto
+	fmt.Print("\n\n*** Simple ***\n\n")
+	sm := doSimple()
+	sm2 := &simplepb.SimpleMessage{}
+	readAndWrite("simple.bin", sm, sm2)
+	fmt.Println("")
+	jsonDemo(sm)
+
+	// enum proto
+	fmt.Print("\n\n*** Enum ***\n\n")
+	en := doEnum(30)
+	fmt.Println(en)
+	en.DayOfTheWeek = enumpb.DayOfTheWeek_FRIDAY
+	fmt.Println(en)
+	en2 := &enumpb.EnumMessage{}
+	readAndWrite("enum.bin", en, en2)
+	fmt.Println("")
+	jsonDemo(en)
+}
+
+func readAndWrite(fileName string, pb proto.Message, pb2 proto.Message) {
+	writeToFile(fileName, pb)
+	readFromFile(fileName, pb2)
+	fmt.Println("PB from", fileName)
+	fmt.Println(pb2)
+}
+
+func jsonDemo(pb proto.Message) {
+	pbAsJSONString := toJson(pb)
+	fmt.Println("PB to JSON:")
+	fmt.Println(string(pbAsJSONString))
+	fromJson(pbAsJSONString, pb)
+	fmt.Println("PB from JSON:", pb)
+}
+
+func writeToFile(fileName string, pb proto.Message) error {
 	out, err := proto.Marshal(pb)
 	if err != nil {
 		log.Fatalln("Can't serialize to bytes", err)
 		return err
 	}
-	if err := ioutil.WriteFile(fName, out, 0644); err != nil {
+	if err := ioutil.WriteFile(fileName, out, 0644); err != nil {
 		log.Fatalln("Can't write to file", err)
 		return err
 	}
 
-	fmt.Println("Data has been written to:", fName)
+	fmt.Println("Data has been written to:", fileName)
 	return nil
 }
 
-func readFromFIle(fName string, pb proto.Message) error {
-	in, err := ioutil.ReadFile(fName)
+func readFromFile(fileName string, pb proto.Message) error {
+	in, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		log.Fatalln("Can't read the file", err)
 		return err
